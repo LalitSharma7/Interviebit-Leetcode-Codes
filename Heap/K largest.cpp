2c9089a89f40c6e795a7da71b0092f7f082209b0
Lalit Sharma <38866385+LalitSharma7@users.noreply.github.com>
2021-07-24 13:22:28 +0530

    Create K largest.cpp

---
+++ Heap/K largest.cpp
@@ -0,0 +1,13 @@
+vector<int> Solution::solve(vector<int> &A, int B) {
+    priority_queue<int, vector<int>> pq;
+    for(int i=0;i<A.size();i++)
+        pq.push(A[i]);
+
+    vector<int> ans;
+    for(int i=0;i<B;i++)
+    {
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    return ans;  
+}
