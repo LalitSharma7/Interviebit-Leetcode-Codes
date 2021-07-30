0bc2681ae930369f7358fe2873ce13d1699c7a1a
Lalit Sharma <38866385+LalitSharma7@users.noreply.github.com>
2021-07-30 12:42:46 +0530

    Update Noble integer.cpp

--- Array/Noble integer.cpp
+++ Array/Noble integer.cpp
@@ -4,6 +4,9 @@ int Solution::solve(vector<int> &A) {
     
     sort(A.begin(), A.end());
     
+    if(A[n-1]==0)
+        return 1;
+    
     for(int i=0;i<n;i++)
     {
         if(A[i]!=A[i+1])
