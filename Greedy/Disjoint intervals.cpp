ccd5db0436e7bed2dc5e357d14e136df708c3323
Lalit Sharma <38866385+LalitSharma7@users.noreply.github.com>
2021-08-10 14:47:08 +0530

    Create Disjoint intervals.cpp

---
+++ Greedy/Disjoint intervals.cpp
@@ -0,0 +1,15 @@
+bool pmj(const vector<int>&a,const vector<int>&b){
+    
+    return a[1]<b[1];
+}
+
+int Solution::solve(vector<vector<int> > &A) {
+    sort(A.begin(),A.end(),pmj);
+    int count=1,j=0;
+    for(int i=1;i<A.size();i++){
+        if(A[j][1]<A[i][0]){
+            count++;j=i;
+        }
+    }
+    return count;
+}
