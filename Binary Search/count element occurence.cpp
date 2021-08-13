e1aacee619ec1eb508253c781bd816b18e6cfd41
Lalit Sharma <38866385+LalitSharma7@users.noreply.github.com>
2021-08-13 13:38:50 +0530

    Create count element occurence.cpp

---
+++ Binary Search/count element occurence.cpp
@@ -0,0 +1,70 @@
+int findright(vector<int>a, int x)
+{
+    int n = a.size();
+    int l=0;
+    int h =n-1;
+    int ans = -1;
+    
+    while(l<=h)
+    {
+        int mid = l+(h-l)/2;
+        
+        if(a[mid]==x)
+        {
+            ans = mid;
+            l = mid+1;
+        }
+        
+        else if(a[mid]>x)
+        {
+            h = mid-1;
+        }
+        
+        else
+        {
+            l= mid+1;
+        }
+    }
+    
+    return ans;
+    
+}
+
+int findleft(vector<int>a,int x)
+{
+    int n = a.size();
+    int l=0;
+    int h=n-1;
+    int ans =-1;
+    
+    while(l<=h)
+    {
+        int mid = l+(h-l)/2;
+        
+        if(a[mid]==x)
+        {
+            ans = mid;
+            h =mid-1;
+        }
+        
+        else if(a[mid]>x)
+        {
+            h=mid-1;
+        }
+        else
+        {
+            l=mid+1;
+        }
+    }
+    
+    return ans;
+}
+
+
+
+int Solution::findCount(const vector<int> &A, int B) {
+    int right = findright(A,B);
+    int left = findleft(A,B);
+    
+    return right-left+1;
+}
