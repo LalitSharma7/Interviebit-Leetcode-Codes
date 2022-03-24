bdd660f899ed366c2c9207f0ebe7b37de91fc9ed
Lalit Sharma <38866385+LalitSharma7@users.noreply.github.com>
2022-03-24 22:36:13 +0530

    Update Last stone weight.cpp

--- Heap/Last stone weight.cpp
+++ Heap/Last stone weight.cpp
@@ -1,1 +1,22 @@
 
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int,vector<int>>q;
+        for(auto x:stones){
+            q.push(x);
+        }
+        while(q.size()>1){
+            int a=q.top();
+            q.pop();
+            int b=q.top();
+            q.pop();
+            if(a!=b){
+                q.push(a-b);
+            }
+        }
+        if(q.size()==NULL) return 0;
+        else return q.top();
+        
+    }
+};
