c9213b6ac7fc2925398caa7be8a838871367e4b9
Lalit Sharma <38866385+LalitSharma7@users.noreply.github.com>
2022-05-20 12:45:49 +0530

    Create check if n and its double exist.cpp

---
+++ Hashing/check if n and its double exist.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        
+        unordered_map<int,int>mp;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            mp[arr[i]]++;
+            if(arr[i]!=0)
+            {
+               if(arr[i]%2==0 && mp[arr[i]/2])
+                      return true;
+            
+                if(mp[arr[i]*2])
+                      return true;
+            }
+        }
+        
+        if(mp[0]&&mp[0]>1)
+            return true;
+        
+        return false;
+        
+    }
