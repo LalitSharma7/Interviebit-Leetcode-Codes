8f2b45dcfc4c387fc20517cab36d42fbf8dfd630
Lalit Sharma <38866385+LalitSharma7@users.noreply.github.com>
2022-05-28 11:59:05 +0530

    Create intersection of arrays.cpp

---
+++ Hashing/intersection of arrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    set<int> s1(nums1.begin(), nums1.end());
+	set<int> s2(nums2.begin(), nums2.end());
+
+	vector<int> v;
+
+	for (auto i: s2) {
+		if (s1.find(i) != s1.end())
+			v.push_back(i);
+	}
+
+	 return v;
+    }
+};
